aaf9271f4a05be906b24cb68fa271a7c272e40bd
Schreibmann <jewishreich@gmail.com>
2017-12-22 23:35:13 +0300

    gh-pages

---
+++ index.js
@@ -0,0 +1,21 @@
+function main () {
+    document.querySelectorAll('.wrapper>div').forEach( panelElement => {
+      var panel = new Panel();
+      panel.init(panelElement);
+      
+  });
+}
+
+function Panel() {
+  this.init = function(el) {
+    el.clicks = 0;
+    el.addEventListener("click", function() {
+         this.clicks++;
+         console.log( this.className + ' was clicked ' + this.clicks + ' times');
+         var counter = document.querySelector('.' + this.className + '-counter');
+         counter.innerHTML = this.clicks;
+    });
+  };
+}
+
+window.onload = main;
