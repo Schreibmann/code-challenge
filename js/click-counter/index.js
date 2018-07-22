ae9c03aaf24839910113e91342da4a4e476d61b1
Schreibmann <jewishreich@gmail.com>
2018-07-22 13:35:38 +0300

    Applied linter & prettier
    Now code looks fine

--- js/click-counter/index.js
+++ js/click-counter/index.js
@@ -1,8 +1,7 @@
-function main () {
-    document.querySelectorAll('.wrapper>div').forEach( panelElement => {
-      var panel = new Panel();
-      panel.init(panelElement);
-      
+function main() {
+  document.querySelectorAll(".wrapper>div").forEach(panelElement => {
+    var panel = new Panel();
+    panel.init(panelElement);
   });
 }
 
@@ -10,10 +9,10 @@ function Panel() {
   this.init = function(el) {
     el.clicks = 0;
     el.addEventListener("click", function() {
-         this.clicks++;
-         console.log( this.className + ' was clicked ' + this.clicks + ' times');
-         var counter = document.querySelector('.' + this.className + '-counter');
-         counter.innerHTML = this.clicks;
+      this.clicks++;
+      console.log(this.className + " was clicked " + this.clicks + " times");
+      var counter = document.querySelector("." + this.className + "-counter");
+      counter.innerHTML = this.clicks;
     });
   };
 }
--- js/curry/index.js
+++ js/curry/index.js
@@ -1,37 +1,37 @@
-let abc = ( a, b, c ) => {
-  return ( a + b + c );
-}
+let abc = (a, b, c) => {
+  return a + b + c;
+};
 
-let abcdef = ( a, b, c, d, e, f ) => {
-  return ( a + b + c + d + e + f );
-}
+let abcdef = (a, b, c, d, e, f) => {
+  return a + b + c + d + e + f;
+};
 
-let curry = ( curried ) => {
-  return function curr1( ...args ) {
-    if ( args.length >= curried.length ) {
-      return curried( ...args )
+let curry = curried => {
+  return function curr1(...args) {
+    if (args.length >= curried.length) {
+      return curried(...args);
     } else {
-        return function curr2( ...moreArgs ) {
-          let newArgs = [...args, ...moreArgs];
-                return curr1( ...newArgs );  
-        }
-      }
+      return function curr2(...moreArgs) {
+        let newArgs = [...args, ...moreArgs];
+        return curr1(...newArgs);
+      };
+    }
+  };
+};
+
+abc.curry = curry(abc);
+abcdef.curry = curry(abcdef);
+
+function assert(a, b) {
+  if (a !== b) {
+    console.log("Test not passed");
+  } else {
+    console.log("Test passed");
   }
 }
 
-abc.curry = curry( abc );
-abcdef.curry = curry( abcdef );
-
-function assert ( a, b ) {
-  if ( a !== b ) {
-    console.log( 'Test not passed' );
-  } else {
-      console.log( 'Test passed' );
-    }
-}
-
-assert(abc.curry('A', 'B', 'C'), 'ABC');
-assert(abc.curry('A')('B')('C'), 'ABC');
-assert(abc.curry('A', 'B')('C'), 'ABC');
-assert(abcdef.curry('A')('B')('C')('D')('E')('F'), 'ABCDEF');
-assert(abcdef.curry('A', 'B', 'C')('D', 'E', 'F'), 'ABCDEF');
+assert(abc.curry("A", "B", "C"), "ABC");
+assert(abc.curry("A")("B")("C"), "ABC");
+assert(abc.curry("A", "B")("C"), "ABC");
+assert(abcdef.curry("A")("B")("C")("D")("E")("F"), "ABCDEF");
+assert(abcdef.curry("A", "B", "C")("D", "E", "F"), "ABCDEF");
